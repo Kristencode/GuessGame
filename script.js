0fcc1116c2e64dc4743875a0c2d9dc8c34000c2f
Kristencode <harunakristen@gmail.com>
2023-04-06 12:29:57 +0100

    all

--- script.js
+++ script.js
@@ -44,6 +44,7 @@
 
 
 const hiddenNo = Math.trunc(Math.random() * 20) + 1;
+//math.ttrunc is used to generate random numbers.
 
 let gameScore = 20
 let highscore = 0
@@ -52,65 +53,64 @@ let highscore = 0
 
 document.querySelector('.check').addEventListener(
     'click', function () {
-        const guess = Number(document.querySelector('.guess').value
+        const guess = Number(document.querySelector('.guess').value // the 'number' is added to convert a string into a number
         );
 
         if (!guess) {
-            document.querySelector('.message').textContent = '!no value';
+            document.querySelector('.message').textContent = '!no value'; //here, if in the game there is no guess, the return message should be 'no value
         }
 
-        else
+        if (guess === hiddenNo) {
+            document.querySelector('.message').textContent = 'Yes, you win'
+            document.querySelector('.highscore').textContent = gameScore
 
-            if (guess === hiddenNo) {
-                document.querySelector('.message').textContent = 'Yes, you win'
+            document.querySelector('.number').textContent = hiddenNo
+            document.querySelector('body').style.backgroundColor = '#20d0d9';
+            document.querySelector('.number').style.width = '40rem'
+            document.querySelector('.number').style.fontSize = '10 rem'
+            document.querySelector('body').style.color = '#333'
+            document.querySelector('.number').style.background = '#eee';// here is when the player wins..all of
+            // the above actions takes place only if player wins
 
-                document.querySelector('.number').textContent = hiddenNo
-                document.querySelector('body').style.backgroundColor = '#505090';
-                document.querySelector('.number').style.width = '40rem'
-                document.querySelector('.number').style.fontSize = '10 rem'
-                document.querySelector('.number').style.background = '#222';
 
-                if (score > highscore) {
-                    highscore = score;
-                    document.querySelector('.highscore').textContent = highscore
-                }
+
+
+        }
+
+        if (guess > hiddenNo) {
+
+            if (gameScore > 1) {
+                document.querySelector('.message').textContent = 'higher number'
+                gameScore--;
+                document.querySelector('.score').textContent = gameScore
 
             }
 
-            else if (guess > hiddenNo) {
-
-                if (gameScore > 1) {
-                    document.querySelector('.message').textContent = 'higher number'
-                    gameScore--;
-                    document.querySelector('.score').textContent = gameScore
-
-                }
-
-                else {
-                    document.querySelector('.message').textContent = 'Sad..You lost!'
-                    document.querySelector('.score').textContent = 0
-                }
+            else {
+                document.querySelector('.message').textContent = 'Sad..You lost!'
+                document.querySelector('.score').textContent = 0
+            } //in instances where guessed number is higher than the hidden number, score reduces and when it gets to zero player loses.
 
 
-            } else
+        }
 
 
-                if (guess < hiddenNo) {
+        if (guess < hiddenNo) {
 
 
-                    if (gameScore > 1) {
-                        document.querySelector('.message').textContent = 'Low number, try again!'
-                        gameScore--;
-                        document.querySelector('.score').textContent = gameScore
+            if (gameScore > 1) {
+                document.querySelector('.message').textContent = 'Low number, try again!'
+                gameScore--;
+                document.querySelector('.score').textContent = gameScore
 
 
-                    } else {
+            } else {
 
-                        document.querySelector('.message').textContent = 'OOps..You lost!'
-                        document.querySelector('.score').textContent = 0
+                document.querySelector('.message').textContent = 'OOps..You lost!'
+                document.querySelector('.score').textContent = 0
 
-                    }
-                }
+            }
+        } //as above so here
 
 
     });
@@ -121,17 +121,24 @@ document.querySelector('.check').addEventListener(
 document.querySelector('.again').addEventListener(
     'click', function () {
         document.querySelector('.score').textContent = 20;
+        document.querySelector('.score').style.color = '#333';
+
         document.querySelector('.number').textContent = '?';
+        document.querySelector('.number').style.width = '15rem';
         document.querySelector('.guess').value = '';
         document.querySelector('.message').textContent = 'Start over again..!'
+        document.querySelector('.message').style.color = '#333';
 
-        document.querySelector('body').style.backgroundColor = '#222';
-        document.querySelector('.number').style.width = '15rem';
-        document.querySelector('.number').style.background = '#eee'
+        document.querySelector('body').style.backgroundColor = '#e7b6b6';
+        document.querySelector('body').style.color = '#333';
+
+        document.querySelector('.highscore').style.color = '#333'
+
+        document.querySelector('.guess').style.background = '#333'
 
 
     }
-)
+)// here is the function for the again button...here the again button when clicked resets all functions back to default.
 
 
 
